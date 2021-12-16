4a5acf63521fdfbb22b845f7ef725ab66e1fc72a
Noah Weaver <noahweaver@aol.com>
2021-12-16 15:44:24 -0600

    backend file work

---
+++ models/issue.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Issue", issueSchema); 
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressJwt = require('express-jwt')
+const PORT = process.env.PORT || 9000
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+
+mongoose.set("useCreateIndex", true)
+mongoose.connect("mongodb://localhost:27017/rtv-db", 
+    {useNewUrlParser: true, useUnifiedTopology: true}, 
+    (err) => {
+        if (err) {
+            console.log(err)
+        }
+    console.log("Connected to the database")
+    }
+)
+
+app.use("/auth", require("./routes/authRouter.js"))
+app.use("/api", expressJwt({ secret: process.env.SECRET, algorithms: ['HS256']}))  //req.user
+app.use("/api/todo", require("./routes/todo.js"))
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({ message: err.message })
+})
+
+app.listen(PORT, () => {
+    console.log(`Starting server on port ${PORT}`)
+})
