372a5f4a2da45b6e828ce04858817a9a59a5546b
Noah Weaver <noahweaver@aol.com>
2021-12-21 14:21:08 -0600

    navigates to dashboard properly

--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
 
 function Issue(props){
-
+  const navigate = useNavigate()
   const {issue, description, _id} = props
 
   return (
@@ -11,8 +13,11 @@ function Issue(props){
       <p>{description}</p>
       
       {/* upvotes and downvotes */}
-      {/* comments */}
+      {/* Only see comments if on issue dashboard */}
 
+    <Button onClick={() => {
+      navigate(`/issue/${_id}`, {state: {issue}})
+    }}>{`Go to ${issue}`}</Button>
     </li>
   )
 }
